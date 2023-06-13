9e8b0933f8b4b2c14d96425d8d9509584d82866e
katexgithub <githubkate@gmail.com>
2023-06-13 17:02:27 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,8 +1,32 @@
 package com.opencart;
 
-public class TestRunner {
-    public static void main(String[] args) {
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 
-        System.out.println("Hello world!");
+public class TestRunner {
+    public static void main(String[] args) throws InterruptedException {
+
+        // Define a driver object that will be used for future actions.
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        driver.get("https://mvnrepository.com/");
+
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://chromedriver.chromium.org/");
+
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+
+        driver.get("https://news.google.com/home?hl=en-US&gl=US&ceid=US:en");
+
+        driver.quit();
+
+        System.out.println("The execution was finished");
+
     }
 }
