07a9087e9bf141778bb0dc6376c5c9f559792bb5
katexgithub <githubkate@gmail.com>
2023-06-16 14:31:03 +0300

    Implemented the FakeDataManager and created the first scenario.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,11 @@
 package com.opencart;
 
+import com.opencart.managers.DataFakerManager;
 import com.opencart.managers.DriverManager;
+import com.opencart.managers.ScrollManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 
 public class TestRunner {
@@ -10,23 +14,63 @@ public class TestRunner {
         // Define a driver object that will be used for future actions.
         WebDriver driver = DriverManager.getInstance().getDriver();
 
-        driver.get("https://mvnrepository.com/");
+        driver.get("https://www.google.com/");
 
         String currentWindowName = driver.getWindowHandle();
 
         driver.switchTo().newWindow(WindowType.TAB);
 
-        driver.get("https://chromedriver.chromium.org/");
+        driver.get("https://andreisecuqa.host/");
+
+        WebElement accountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        accountIcon.click();
+
+        WebElement registerBtn = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
+        registerBtn.click();
+
+        String firstName = DataFakerManager.getRandomName();
+        System.out.println("The generated First Name is: " + firstName);
+
+        String lastName = DataFakerManager.getRandomName();
+        System.out.println("The generated Last Name is: " + lastName);
+
+        String randomEmail = DataFakerManager.getRandomEmail("john", "@gmail.com");
+        System.out.println("The generated Email is: " + randomEmail);
+
+        String password = DataFakerManager.getRandomPassword(10, 30);
+        System.out.println("The generated Password is: " + password);
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.sendKeys(firstName);
+
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.sendKeys(lastName);
+
+        WebElement emailInput = driver.findElement(By.id("input-email"));
+        emailInput.sendKeys(randomEmail);
+
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.sendKeys(password);
+
+        WebElement privacyToggle = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
+        ScrollManager.scrollToElement(driver, privacyToggle);
+        privacyToggle.click();
+
+        WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
+        continueButton.click();
+
+        Thread.sleep(5000);
+
+        System.out.println(driver.getCurrentUrl());
 
         driver.close();
 
         driver.switchTo().window(currentWindowName);
 
-        driver.get("https://news.google.com/home?hl=en-US&gl=US&ceid=US:en");
+        driver.get("https://andreisecuqa.host/");
 
         driver.quit();
 
         System.out.println("The execution was finished");
-
     }
 }
