4586082367277d1757b1b59ff58455b8f18b1e3c
katexgithub <githubkate@gmail.com>
2023-06-20 08:52:30 +0300

    This commit introduces the implementation of PageObjects and PageFactory design patterns to improve test automation efficiency and maintainability. PageObjects are used to encapsulate the elements and actions on web pages, while PageFactory is employed to initialize and manage the elements within PageObjects.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -2,75 +2,38 @@ package com.opencart;
 
 import com.opencart.managers.DataFakerManager;
 import com.opencart.managers.DriverManager;
-import com.opencart.managers.ScrollManager;
-import org.openqa.selenium.By;
+import com.opencart.pageobjects.AccountCreatedPage;
+import com.opencart.pageobjects.HomePage;
+import com.opencart.pageobjects.RegisterPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
-
-        // Define a driver object that will be used for future actions.
         WebDriver driver = DriverManager.getInstance().getDriver();
-
-        driver.get("https://www.google.com/");
-
-        String currentWindowName = driver.getWindowHandle();
-
-        driver.switchTo().newWindow(WindowType.TAB);
-
         driver.get("https://andreisecuqa.host/");
 
-        WebElement accountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
-        accountIcon.click();
-
-        WebElement registerBtn = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
-        registerBtn.click();
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegisterPageFromHeaderMenu();
 
         String firstName = DataFakerManager.getRandomName();
-        System.out.println("The generated First Name is: " + firstName);
-
         String lastName = DataFakerManager.getRandomName();
-        System.out.println("The generated Last Name is: " + lastName);
-
         String randomEmail = DataFakerManager.getRandomEmail("john", "@gmail.com");
-        System.out.println("The generated Email is: " + randomEmail);
-
         String password = DataFakerManager.getRandomPassword(10, 30);
-        System.out.println("The generated Password is: " + password);
 
-        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
-        firstNameInput.sendKeys(firstName);
-
-        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
-        lastNameInput.sendKeys(lastName);
-
-        WebElement emailInput = driver.findElement(By.id("input-email"));
-        emailInput.sendKeys(randomEmail);
-
-        WebElement passwordInput = driver.findElement(By.id("input-password"));
-        passwordInput.sendKeys(password);
-
-        WebElement privacyToggle = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
-        ScrollManager.scrollToElement(driver, privacyToggle);
-        privacyToggle.click();
-
-        WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
-        continueButton.click();
-
-        Thread.sleep(5000);
-
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.fillInTheRegisterForm(firstName, lastName, randomEmail, password);
+        registerPage.switchOnThePrivacyToggle(driver);
+        registerPage.clickOnTheContinueButton();
+        Thread.sleep(2000);
         System.out.println(driver.getCurrentUrl());
 
-        driver.close();
+        AccountCreatedPage accountCreatedPage = new AccountCreatedPage(driver);
+        accountCreatedPage.logOutFromTheAccount();
+        Thread.sleep(1000);
+        System.out.println(driver.getCurrentUrl());
 
-        driver.switchTo().window(currentWindowName);
-
-        driver.get("https://andreisecuqa.host/");
-
+        Thread.sleep(2000);
         driver.quit();
-
         System.out.println("The execution was finished");
     }
 }
